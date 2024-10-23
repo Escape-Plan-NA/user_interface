83c6c54abc6a1e0d4961d581ed8e1874e998047f
Oshin-hi <74412100+Oshin-hi@users.noreply.github.com>
2024-10-23 21:40:28 +0700

    Back button in all pages except landing page
    -Created a layout to globally put back button

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate, useLocation, useNavigate } from "react-router-dom";
 import Menu from "./pages/MainMenu/Menu.jsx";
 import Profile from "./pages/Profile/Profile.jsx";
 import Setting from "./pages/Settings/Settings.jsx";
@@ -9,27 +9,65 @@ import BotGameplay from "./pages/GamePlay/BotGameplay.jsx";
 import Cutscene from "./pages/CutScene/Cutscene.jsx";
 import SingleCutscene from "./pages/CutScene/SingleplayerCutscene.jsx";
 import Lobby from "./pages/Lobby/Lobby.jsx";
-import SingleplayerModeSelection from "./pages/Selection/SingleplayerModeSelection.jsx"; // Import the new page
+import SingleplayerModeSelection from "./pages/Selection/SingleplayerModeSelection.jsx"; 
+import { FaArrowLeft } from 'react-icons/fa'; 
+
+// Create a global layout to manage the back button
+const Layout = ({ children }) => {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // Function to go back to the previous page
+  const handleBack = () => {
+    navigate(-1); // Navigate to the previous route
+  };
+
+  // Don't show the Back button on the home (Menu) page
+  const shouldShowBackButton = location.pathname !== "/";
+
+  return (
+    <div>
+      {/* Conditionally render the Back button */}
+      {shouldShowBackButton && (
+        <button onClick={handleBack} style={{ position: "fixed", top: 10, left: 10 }}>
+          <FaArrowLeft />
+        </button>
+      )}
+
+      {/* Render the page content */}
+      <div>{children}</div>
+    </div>
+  );
+};
+
+const buttonStyle = {
+  position: "fixed",
+  top: 10,
+  left: 10,
+  background: "transparent",
+  border: "none",
+  fontSize: "24px",
+  cursor: "pointer"
+};
 
 const App = () => {
   return (
     <Router>
-      <Routes>
-        {/* Route for the Menu page */}
-        <Route path="/" element={<Menu />} />
-        <Route path="/setting" element={<Setting />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/singledevice" element={<SingleplayerModeSelection />} />
-        <Route path="/friends" element={<SingleGameplay />} />
-        <Route path="/bot" element={<BotGameplay />} />
-        <Route path="/singlecutscene" element={<SingleCutscene />} />
-        <Route path="/cutscene" element={<Cutscene />} />
-        <Route path="/lobby" element={<Lobby />} />
-        <Route path="/start" element={<GamePlay />} />
-        
-        {/* Redirect all other undefined routes to the Menu page */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Menu />} />
+          <Route path="/setting" element={<Setting />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/singledevice" element={<SingleplayerModeSelection />} />
+          <Route path="/friends" element={<SingleGameplay />} />
+          <Route path="/bot" element={<BotGameplay />} />
+          <Route path="/singlecutscene" element={<SingleCutscene />} />
+          <Route path="/cutscene" element={<Cutscene />} />
+          <Route path="/lobby" element={<Lobby />} />
+          <Route path="/start" element={<GamePlay />} />
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 };
