8c3ba64af082b00c3f04156a4f55b0da5b3b3250
Squidisverycool <konun893@gmail.com>
2024-10-28 08:30:13 +0700

    I love numbers, that's why I keep fucking them up

--- src/pages/GamePlay/Gameplay.jsx
+++ src/pages/GamePlay/Gameplay.jsx
@@ -314,7 +314,7 @@ useEffect(() => {
     }
   }, [gameTimer]);
   useEffect(() => {
-    if (turnTimer <= 1) {
+    if (turnTimer <= 0) {
         switchTurns(); // Call switch turns if time runs out
     }
   }, [turnTimer]);
